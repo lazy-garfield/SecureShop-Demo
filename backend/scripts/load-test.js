4d43cb816f23444b417ad02f1d75c87b00eea01f
lazy-garfield <vinithv1462@gmail.com>
2026-03-26 11:08:46 +0530

    Add files via upload

---
+++ backend/scripts/load-test.js
@@ -0,0 +1,11 @@
+const autocannon = require("autocannon");
+
+const url = process.argv[2] || "http://localhost:4000/api/products";
+
+const instance = autocannon({
+  url,
+  connections: 20,
+  duration: 10
+});
+
+autocannon.track(instance, { renderProgressBar: true });
